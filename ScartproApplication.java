4c4de86a2c678967b68e40af5ec583a9432c804b
sstulip <86274144+sstulip@users.noreply.github.com>
2023-06-30 03:33:06 -0700

    Add files via upload

---
+++ ScartproApplication.java
@@ -0,0 +1,13 @@
+package com.sample.scartpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScartproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScartproApplication.class, args);
+	}
+
+}
